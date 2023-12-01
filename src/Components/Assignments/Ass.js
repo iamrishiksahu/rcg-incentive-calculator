6b835fa251210efb6f95f6edb309645ffca74e0c
Rishik Sahu <rishikkr@hotmail.com>
2023-12-01 14:17:53 +0530

    modified table

--- src/Components/Assignments/Ass.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Ass = () => {
-  return (
-    
-  )
-}
-
-export default Ass
--- src/Components/Assignments/AssignmentDashboardTable.js
+++ src/Components/Assignments/AssignmentDashboardTable.js
@@ -6,20 +6,13 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import './AssignmentDashboardTable.css'
+import { Link, useNavigate } from 'react-router-dom';
+import { Typography } from '@mui/material';
 
 
 const data = [{
-  candidate_first_name: 'Rishik',
-  candidate_last_name: 'Sahu',
-  bill_start: '22-10-2023',
-  bill_end: '22-10-2023',
-  assignment_status: 'Assigned',
-  job_title: 'Web Developer',
-  working_state: 'Jharkhand',
-  assignment_division: 'IT',
-  client_contact: 'Bhavya B.',
-
-},{
+  id: '1',
   candidate_first_name: 'Rishik',
   candidate_last_name: 'Sahu',
   bill_start: '22-10-2023',
@@ -31,8 +24,12 @@ const data = [{
   working_state: 'Jharkhand',
   assignment_division: 'IT',
   client_contact: 'Bhavya B.',
-
-},{
+  primary_recruiter: 'Ravi K V',
+  primary_sales: '34',
+  start_entered: '24-11-2000',
+  optional_ref: '124582'
+}, {
+  id: '1',
   candidate_first_name: 'Rishik',
   candidate_last_name: 'Sahu',
   bill_start: '22-10-2023',
@@ -44,8 +41,12 @@ const data = [{
   working_state: 'Jharkhand',
   assignment_division: 'IT',
   client_contact: 'Bhavya B.',
-
-},{
+  primary_recruiter: 'Ravi K V',
+  primary_sales: '34',
+  start_entered: '24-11-2000',
+  optional_ref: '124582'
+}, {
+  id: '1',
   candidate_first_name: 'Rishik',
   candidate_last_name: 'Sahu',
   bill_start: '22-10-2023',
@@ -57,53 +58,43 @@ const data = [{
   working_state: 'Jharkhand',
   assignment_division: 'IT',
   client_contact: 'Bhavya B.',
-
-},{
-  candidate_first_name: 'Rishik',
-  candidate_last_name: 'Sahu',
-  bill_start: '22-10-2023',
-  pay_start: '22-10-2023',
-  bill_end: '22-10-2023',
-  pay_end: '22-10-2023',
-  assignment_status: 'Assigned',
-  job_title: 'Web Developer',
-  working_state: 'Jharkhand',
-  assignment_division: 'IT',
-  client_contact: 'Bhavya B.',
-  
+  primary_recruiter: 'Ravi K V',
+  primary_sales: '34',
+  start_entered: '24-11-2000',
+  optional_ref: '124582'
 },
 ]
 
 const rows = data.map((item) => {
-  return {...item, name: item.candidate_first_name + " " + item.candidate_last_name}
+  return { ...item, name: item.candidate_first_name + " " + item.candidate_last_name }
 })
 
 const AssignmentDashboardTable = () => {
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableContainer component={Paper} className='table-container'>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table" >
         <TableHead>
           <TableRow>
             <TableCell sx={{
-                position: 'sticky',
-                left: 0,
-                background: '#f0f0f0',
-                minWidth: '200px',
+              position: 'sticky',
+              left: 0,
+              background: '#fff',
+              minWidth: '200px',
             }}>Candidate Name</TableCell>
-            <TableCell align="center" sx={{minWidth: '130px'}}>Bill Start</TableCell>
-            <TableCell align="center" sx={{minWidth: '130px'}}>Bill End</TableCell>
-            <TableCell align="center" sx={{minWidth: '100px'}}>Pay Start</TableCell>
-            <TableCell align="center" sx={{minWidth: '100px'}}>Pay End</TableCell>
-            <TableCell align="center" sx={{minWidth: '150px'}}>Assignment Status</TableCell>
-            <TableCell align="center" sx={{minWidth: '100px'}}>Job Title</TableCell>
-            <TableCell align="center" sx={{minWidth: '100px'}}>Division</TableCell>
-            <TableCell align="center" sx={{minWidth: '150px'}}>Working State</TableCell>
-            <TableCell align="center" sx={{minWidth: '150px'}}>Client Contact</TableCell>
-            <TableCell align="center" sx={{minWidth: '150px'}}>Primary Recruiter</TableCell>
-            <TableCell align="center" sx={{minWidth: '150px'}}>Primary Sales</TableCell>
-            <TableCell align="center" sx={{minWidth: '150px'}}>Start Entered</TableCell>
-            <TableCell align="center" sx={{minWidth: '100px'}}>Reference</TableCell>
-center
+            <TableCell align="center" sx={{ minWidth: '120px' }}>Bill Start</TableCell>
+            <TableCell align="center" sx={{ minWidth: '120px' }}>Bill End</TableCell>
+            <TableCell align="center" sx={{ minWidth: '120px' }}>Pay Start</TableCell>
+            <TableCell align="center" sx={{ minWidth: '120px' }}>Pay End</TableCell>
+            <TableCell align="center" sx={{ minWidth: '150px' }}>Assignment Status</TableCell>
+            <TableCell align="center" sx={{ minWidth: '100px' }}>Job Title</TableCell>
+            <TableCell align="center" sx={{ minWidth: '100px' }}>Division</TableCell>
+            <TableCell align="center" sx={{ minWidth: '150px' }}>Working State</TableCell>
+            <TableCell align="center" sx={{ minWidth: '150px' }}>Client Contact</TableCell>
+            <TableCell align="center" sx={{ minWidth: '150px' }}>Primary Recruiter</TableCell>
+            <TableCell align="center" sx={{ minWidth: '150px' }}>Primary Sales</TableCell>
+            <TableCell align="center" sx={{ minWidth: '150px' }}>Start Entered</TableCell>
+            <TableCell align="center" sx={{ minWidth: '120px' }}>Optional Ref #</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -115,10 +106,13 @@ center
               <TableCell component="th" scope="row" sx={{
                 position: 'sticky',
                 left: 0,
-                background: '#f0f0f0',
-                
+                backgroundColor: '#f0eded',
               }}>
-                {row.name}
+
+                <Link style={{color: 'rgba(0, 0, 0, 0.87)'}} to={(`/dashboard/add-assignment/${row?.id}`)} >
+                  {row.name}
+
+                </Link>
               </TableCell>
               <TableCell align="center">{row.bill_start}</TableCell>
               <TableCell align="center">{row.bill_end}</TableCell>
@@ -129,6 +123,10 @@ center
               <TableCell align="center">{row.assignment_division}</TableCell>
               <TableCell align="center">{row.working_state}</TableCell>
               <TableCell align="center">{row.client_contact}</TableCell>
+              <TableCell align="center">{row.primary_recruiter}</TableCell>
+              <TableCell align="center">{row.primary_sales}</TableCell>
+              <TableCell align="center">{row.start_entered}</TableCell>
+              <TableCell align="center">{row.optional_ref}</TableCell>
             </TableRow>
           ))}
         </TableBody>
